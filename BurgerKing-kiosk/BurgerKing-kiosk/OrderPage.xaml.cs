1b9ad5d4c6bbd2966c28aa38876a7f181b27c399
kimjw2003 <kimjw111003@gmail.com>
2020-09-22 14:42:16 +0900

    orderpage 추가

--- BurgerKing-kiosk/BurgerKing-kiosk/OrderPage.xaml.cs
+++ BurgerKing-kiosk/BurgerKing-kiosk/OrderPage.xaml.cs
@@ -15,9 +15,12 @@ using System.Windows.Shapes;
 
 namespace BurgerKing_kiosk
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// OrderPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class OrderPage : Page
     {
-        public MainWindow()
+        public OrderPage()
         {
             InitializeComponent();
         }
