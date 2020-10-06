0c9beaa1b2a42da7186287e6b443f803168b2c5f
sbear123 <sbear123@naver.com>
2020-10-06 14:37:41 +0900

    [UPDATE] OrderModel 틀

--- BurgerKing-kiosk/BurgerKing-kiosk/model/OrderModel.cs
+++ BurgerKing-kiosk/BurgerKing-kiosk/model/OrderModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace BurgerKing_kiosk.model
 {
     class OrderModel
     {
+        private List<FoodModel> foods;
+        private int table = 0;
+        
+        public void AddFood(int id)
+        {
+            
+        }
+        public void DeleteAll()
+        {
+            this.foods = null;
+            this.table = 0;
+        }
+        public List<FoodModel> Delete(int id)
+        {
+            return foods;
+        }
+        public bool CheckTable(int id)
+        {
+            if (table != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Table(int id)
+        {
+            this.table = id;
+        }
     }
 }
