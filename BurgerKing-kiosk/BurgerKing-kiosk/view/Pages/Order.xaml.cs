8145821dbff52ef089cde55212db9c54a2ef32e9
kimjw2003 <kimjw111003@gmail.com>
2020-10-07 11:50:12 +0900

    [ADD] 리스트 바꾸기 가능

--- BurgerKing-kiosk/BurgerKing-kiosk/view/Pages/Order.xaml.cs
+++ BurgerKing-kiosk/BurgerKing-kiosk/view/Pages/Order.xaml.cs
@@ -41,9 +41,18 @@ namespace BurgerKing_kiosk
             new Food() { category = Category.BURGER, title = "기네스와퍼팩3", imageData = "/view/Images/burger.png" }, 
             new Food() { category = Category.BURGER, title = "와퍼", imageData = "/view/Images/burger.png" }, 
             new Food() { category = Category.BURGER, title = "불고기와퍼", imageData = "/view/Images/burger.png" },
-            new Food() { category = Category.BURGER, title = "치즈와퍼", imageData = "/view/Images/burger.png" }, 
+            new Food() { category = Category.BURGER, title = "치즈와퍼", imageData = "/view/Images/burger.png" },
+
             new Food() { category = Category.SIDE,   title = "치킨", imageData="/view/Images/side.png"}
         };
+
+        private void lbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbCategory.SelectedIndex == -1) return;
+
+            Category category = (Category)lbCategory.SelectedIndex;
+            List.ItemsSource = Food.Where(x => x.category == category).ToList();
+        }
 
         private void OrderPage_Loaded(object sender, RoutedEventArgs e) {
             
