e2c9aa3a8156acc1a27a988a9e9afc4d3cd29eee
bees3030 <bees_3030@naver.com>
2020-10-14 12:10:26 +0900

    [ADD] Sale model

---
+++ BurgerKing-kiosk/BurgerKing-kiosk/model/SaleModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerKing_kiosk.model
+{
+    class SaleModel
+    {
+        public SaleCategory Salecategory { get; set; }
+        public int idx { get; set; }
+        public String menu { get; set; }
+        public String category { get; set; }
+        public String user { get; set; }
+        public DateTime day { get; set; }
+        public int price { get; set; }
+        public int seat { get; set; }
+        public int sale { get; set; }
+
+    }
+    public enum SaleCategory
+    {
+        MENU,
+        CATEGORY,
+        SEAT,
+        DAY,
+        TIME,
+        USER,
+    }
+}
