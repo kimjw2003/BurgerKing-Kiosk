77a901215c4304cd9f435ca8c17800d4f3b2d9cc
Park jihyun <52943905+sbear123@users.noreply.github.com>
2020-11-30 19:06:52 +0900

    [ADD] SERVER 연결되었는지 아닌지 사용자에게 보여줌

--- BurgerKing-kiosk/BurgerKing-kiosk/model/DB/connectDB.cs
+++ BurgerKing-kiosk/BurgerKing-kiosk/model/DB/connectDB.cs
@@ -10,11 +10,16 @@ namespace BurgerKing_kiosk.viewModel.DB
 {
     class connectDB
     {
-        private String ip = "10.80.162.51";
+        //private String ip = "10.80.162.51";
+        //private String port = "3306";
+        //private String dbName = "kiosk";
+        //private String id = "user1";
+        //private String pwd = "1q2w3e4r!";
+        private String ip = "localhost";
         private String port = "3306";
         private String dbName = "kiosk";
-        private String id = "user1";
-        private String pwd = "1q2w3e4r!";
+        private String id = "root";
+        private String pwd = "1234";
         public MySqlConnection conn;
  
         public MySqlConnection OpenConnection()
---
+++ BurgerKing-kiosk/BurgerKing-kiosk/view/Pages/Converter/TextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BurgerKing_kiosk.view.Pages.Converter
+{
+    class TextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? "서버연결 성공" : "서버연결 실패";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
